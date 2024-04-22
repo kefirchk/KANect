07df73a62a5db531002d53f32ca1a0d19cfff515
Alexei Klimovich <prostolex2004@mail.ru>
2024-04-22 22:23:41 +0300

    Added dependency Newtonsoft.Json from Nuget

--- ConsoleClient/Message.cs
+++ ConsoleClient/Message.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace KANect
 {
+    [Serializable]
     public class Message
     {
         public string UserName { get; set; }
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace KANect
 {
@@ -7,7 +8,15 @@ namespace KANect
         static void Main(string[] args)
         {
             Message msg = new Message();
+            string output = JsonConvert.SerializeObject(msg);
+            //{ "UserName":"System","MessageText":"Server is running...","TimeStamp":"2024-04-22T22:21:43.8230604+03:00"}
+
             Console.WriteLine(msg.ToString());
+            Console.WriteLine(output);
+
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+
+            Console.WriteLine(deserializedMsg);
         }
     }
 }
